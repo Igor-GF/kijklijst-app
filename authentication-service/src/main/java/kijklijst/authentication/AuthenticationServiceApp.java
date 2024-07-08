461acee193b322ff38325b631f285358d611bd29
Igor-GF <igfigueiredo@outlook.com>
2024-07-08 16:29:39 +0200

    main methods added to the services

---
+++ authentication-service/src/main/java/kijklijst/authentication/AuthenticationServiceApp.java
@@ -0,0 +1,12 @@
+package kijklijst.authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationServiceApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthenticationServiceApp.class, args);
+    }
+}
---
+++ movie-service/src/main/java/kijklijst/movie/MovieServiceApp.java
@@ -0,0 +1,11 @@
+package kijklijst.movie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieServiceApp {
+    public static void main(String[] args) {
+        SpringApplication.run(MovieServiceApp.class, args);
+    }
+}
